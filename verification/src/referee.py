ced20749fa31b939195ca763eebaf3befe0627bd
Alexander Lyabah <a.lyabah@checkio.org>
2019-03-26 19:11:26 +0200

    first commit

---
+++ verification/src/referee.py
@@ -0,0 +1,16 @@
+from checkio_referee import RefereeRank
+
+
+import settings_env
+from tests import TESTS
+
+
+class Referee(RefereeRank):
+    TESTS = TESTS
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "checkio"
+    FUNCTION_NAMES = {
+        "python_3": "checkio",
+        "js_node": "nonUniqueElements"
+    }
